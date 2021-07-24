78fa7c40ae6a9c8973c109e3258ddeddd93adfb6
Krish <vamsikrish.49@gmail.com>
2021-07-24 22:25:40 +0530

    EagerSingleton Ex

---
+++ Singleton/EagerSingleton.java
@@ -0,0 +1,27 @@
+/*
+ * This is Eager initialization singleton design pattern.
+ * The instance is created at the time of class loading itself.
+ * The drawback in this is even though client doesn't want an instance an instance will be created while class loading itself.
+ */
+class EagerInitializationSingleton {
+
+	private static EagerInitializationSingleton instance = new EagerInitializationSingleton();
+
+	// private constructor so that client can't call this
+	private EagerInitializationSingleton() {
+
+	}
+
+	public static EagerInitializationSingleton getInstance() {
+		return instance;
+	}
+}
+
+public class EagerSingleton {
+
+	public static void main(String args[]) {
+		EagerInitializationSingleton instance1 = EagerInitializationSingleton.getInstance();
+		EagerInitializationSingleton instance2 = EagerInitializationSingleton.getInstance();
+		System.out.println(instance1.hashCode() == instance2.hashCode());
+	}
+}
